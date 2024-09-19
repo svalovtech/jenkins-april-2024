b4a9fcc4c26895bc7ecc9ae2782f9f727f41fb8e
svalovtech <svalovtech@gmail.com>
2024-09-19 11:44:08 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-region = "us-east-2"
+region = var.region
 }
 
 
 resource "aws_instance" "web" {
-ami = "ami-037774efca2da0726"
+ami = var.ami_id
 instance_type = "t2.micro"
 availability_zone = "us-east-2a"
 key_name = "my-laptop-key"
