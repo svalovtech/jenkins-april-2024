4e380f6bb56655d32f8fa2076790f60965a1671f
svalovtech <svalovtech@gmail.com>
2024-09-19 12:37:53 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,11 @@ variable "ami_id" {
 variable "region" {
   description = "AWS region"
 }
+
+locals {
+  availability_zone = {
+    "us-east-1" = "us-east-1a"
+    "us-east-2" = "us-east-2a"
+    "us-west-1" = "us-west-1a"
+    "us-west-2" = "us-west-2a"
+  }[var.region]
