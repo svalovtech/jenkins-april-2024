90cf251533f7e36058c0b2dd2614bcbe9b2bddcf
svalovtech <svalovtech@gmail.com>
2024-09-19 12:59:54 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,7 +10,7 @@ locals {
   availability_zone = {
     "us-east-1" = "us-east-1a"
     "us-east-2" = "us-east-2a"
-    "us-west-1" = "us-west-1a"
-    "us-west-2" = "us-west-2a"
+    "us-west-1" = "us-west-1b"
+    "us-west-2" = "us-west-2c"
   }
 }
